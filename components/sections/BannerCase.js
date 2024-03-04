b7669a72f71339c1a5fe0835955f4d26a35ef911
Mateus Lima <65178646+DxmLima@users.noreply.github.com>
2024-03-04 12:19:26 -0300

    Update BannerCase.js

--- components/sections/BannerCase.js
+++ components/sections/BannerCase.js
@@ -5,7 +5,7 @@ import Spline from '@splinetool/react-spline';
 export default function Banner4() {
     return (
         <>
-            <section className="banner__area-four banner__bg-four" data-background="/assets/img/banner/bgfolio.png">
+           <section className="banner__area-four banner__bg-four" data-background="/assets/img/banner/bgfolio.png">
                 <div className="container">
                     <div className="row align-items-center justify-content-center">
                         <div className="col-lg-6">
@@ -16,7 +16,7 @@ export default function Banner4() {
                             </div>
                         </div>
                         <div className=" d-flex justify-content-center ">
-                            <div className="pt-3 banner__img-two d-none d-lg-block ">
+                            <div className="pt-3 banner__img-two  ">
                             <img src="/assets/img/banner/casetest.png" alt="test" />                                
                             </div>
                         </div>
@@ -54,26 +54,26 @@ export default function Banner4() {
                           <div class="  text-left justify-content-center space-2 ">
                             <div class="row">
                                 <div class="col">
-                                <h3 className='text-white'> My Progress </h3>
-                                    <p className='text-secondary'> <span> Developer | Code, fix design and images</span></p>
-
-                                 <div className='pt-2'> 
-
-                                 <h3 className='text-white'> Timeline </h3>
-                                <p className='text-secondary'> 1 Month </p>
-                                 </div>
+                                <h3 className='text-white'> Initial Thinking </h3>
+                                <h6 className='text-white'> Define target users </h6>
+                                    <p className='text-secondary'> Customers who will use the website to consult store information</p>
                                 </div>
+
                                 <div class="col order-5">
-                                <h3 className='text-white'> OverView </h3>
-                                 <p className='text-secondary text-left'>Developing a landing page to improve the 
-                                                                aesthetics of the website and the user experience, including a 
-                                                                administrative panel to facilitate the registration of new stores, 
-                                                                together with their respective information, in accordance with the 
-                                                                customer needs.</p>
+                                <h3 className='text-white'> Research Methods </h3>
+                                 <p className='text-secondary text-left'>Find the best way to provide performance so that customers can easily find the nearest store and the website has well-defined products, avoiding any type of doubt.</p>
                                 </div>
+                              
                             </div>
                             </div>
                         
+
+                            <div className=" d-flex justify-content-center space">
+                            <video width="930" height="520" autoPlay muted loop>
+                                    <source src="/assets/video/case01.mp4" type="video/mp4" />
+                                    Your browser does not support the video tag.
+                                    </video>                              
+                        </div>
                                             
                     </div>
                 </div>
