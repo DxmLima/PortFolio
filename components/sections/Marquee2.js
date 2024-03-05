047b0aced1f42226765092f3738972db3ebd5b62
Mateus Lima <65178646+DxmLima@users.noreply.github.com>
2024-03-05 15:26:38 -0300

    fix

--- components/sections/Marquee2.js
+++ components/sections/Marquee2.js
@@ -5,17 +5,12 @@ export default function Marquee2() {
         <>
             <div className="marquee__area-two bg-black">
                 <div className="marquee__wrap">
+                    
                     <div className="marquee__box">
-                        <Link data-hover="Marketing" href="/services-details">Website</Link>
-                        <Link data-hover="Finance Advisor" href="/services-details">Design</Link>
-                        <Link data-hover="Investment" href="/services-details">Code</Link>
-                        <Link data-hover="Target" href="/services-details">Deploy</Link>
-                    </div>
-                    <div className="marquee__box">
-                        <Link data-hover="Marketing" href="/services-details">Website</Link>
-                        <Link data-hover="Finance Advisor" href="/services-details">Design</Link>
-                        <Link data-hover="Investment" href="/services-details">Code</Link>
-                        <Link data-hover="Target" href="/services-details">Deploy</Link>
+                        <div data-hover="Marketing">Website</div>
+                        <div data-hover="Finance Advisor">Design</div>
+                        <div data-hover="Investment">Code</div>
+                        <div data-hover="Target">Deploy</div>
                     </div>
                 </div>
             </div>
